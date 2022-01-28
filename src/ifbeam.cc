26df764e7847473d299e6de179521c14e3794283
Lynn Garren <garren@fnal.gov>
2022-01-28 16:29:09 -0600

    fix typo

--- src/ifbeam.cc
+++ src/ifbeam.cc
@@ -73,7 +73,7 @@ BeamFolder::BeamFolder(std::string bundle_name, std::string url, double time_wid
     _values_column = -1;
 
      // make sure we have initialized libwda...
-     (void*)wda_init_cleanup_singleton::get_wda_init_cleanup_singleton();
+     (void)wda_init_cleanup_singleton::get_wda_init_cleanup_singleton();
 
     // pass info about us down for UserAgent: string
     std::stringstream uabuf;
